259ff8ef82f95efb772659cc174c7c97411568d2
Alex Cseke <stefan.cseke@stud.ubbcluj.ro>
2024-12-06 21:10:28 +0200

    removed old server constructor

--- FTPServer/FTPServer.h
+++ FTPServer/FTPServer.h
@@ -16,23 +16,7 @@ private:
     void handleStor() {}
 
 public:
-    FTPServer(std::string port) :
-        m_control_socket(),
-        m_port(port) {
-
-        sockaddr_in serverAddr{};
-        serverAddr.sin_family = AF_INET;
-        serverAddr.sin_port = htons(m_port);
-        serverAddr.sin_addr.s_addr = INADDR_ANY;
-
-        if (bind(m_control_socket.get(),
-            reinterpret_cast<sockaddr*>(&serverAddr),
-            sizeof(serverAddr)) == SOCKET_ERROR) {
-            throw std::runtime_error("Bind failed");
-        }
-
-        listen(m_control_socket.get(), SOMAXCONN);
-    }
+    FTPServer(std::string port);
 
     void start();
 };
